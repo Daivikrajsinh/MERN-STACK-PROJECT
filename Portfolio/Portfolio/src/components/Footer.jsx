fc5e524947f0fe3c594122087c2410c6a56b4b6c
Daivikrajsinh <devikrajsinh05@gmail.com>
2025-09-17 13:00:57 +0530

    portfolio

---
+++ Portfolio/Portfolio/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <footer class="footer">
+    <p class="footer-name">Daivikrajsinh Vaghela</p>
+  
+</footer>
+
+    </>
+  )
+}
+
+export default Footer
